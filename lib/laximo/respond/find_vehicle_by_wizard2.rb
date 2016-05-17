04cf91b8719866516ea53cf9ffdeac21e9af669a
Anton Krichevskii <mrskadi0@gmail.com>
2016-05-17 10:28:54 +0300

    Fix FindVehicleByWizard2 respond

--- lib/laximo/respond/find_vehicle_by_wizard2.rb
+++ lib/laximo/respond/find_vehicle_by_wizard2.rb
@@ -7,7 +7,7 @@ module Laximo
 
       def parsing_result(str)
 
-        str.xpath('//FindVehicleByWizard/row').inject([]) { |arr, node|
+        str.xpath('//FindVehicleByWizard2/row').inject([]) { |arr, node|
 
           h = node_to_hash(node) { |h1, n1|
             h1[:attributes] = nodes_to_hash(n1.xpath('./attribute'))
